cd189a66ad6bae24de41710db378230b05055479
Andre Lobato <andrefelipelobato@gmail.com>
2026-02-15 21:41:05 +1300

    Tokyonight transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,12 +3,22 @@ return {
   { "ellisonleao/gruvbox.nvim" },
   { "Old-Farmer/noctis-nvim" },
   { "scottmckendry/cyberdream.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "cyberdream",
+      colorscheme = "tokyonight",
     },
   },
 }
