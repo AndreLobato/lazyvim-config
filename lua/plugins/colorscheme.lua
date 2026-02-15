c1c755354692dab6267b848d016b2d048b70ffb7
Andre Lobato <andrefelipelobato@gmail.com>
2026-02-15 20:11:26 +1300

    Add cyberdream

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,12 +2,13 @@ return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
   { "Old-Farmer/noctis-nvim" },
+  { "scottmckendry/cyberdream.nvim" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "noctis-obscuro",
+      colorscheme = "cyberdream",
     },
   },
 }
