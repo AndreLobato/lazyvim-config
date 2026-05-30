186f68d3d6794241e23ac9b1af77efcacc0c4258
Andre Lobato <andrefelipelobato@gmail.com>
2026-05-30 12:14:20 +1200

    Smaller explorer width

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,13 +1,28 @@
 return {
   "folke/snacks.nvim",
   opts = {
-    explorer = {},
-    picker = {
+     picker = {
       sources = {
         files = { hidden = true, ignored = false, exclude={ "**/__pycache__/", ".cache" }},
         grep = { hidden = true, ignored = false},
-        explorer = { hidden = true, ignored = true},
+        explorer = {
+          hidden = true,
+          ignored = true,
+          layout = function()
+            return {
+              preset = 'sidebar',
+              preview = false,
+              layout = {
+                  width = (vim.g.explorer_size or 25),
+              },
+            }
+          end,
+          on_close = function(picker)
+            vim.g.explorer_size = picker.layout.root:size().width
+          end
+        },
       },
     },
   },
 }
+
