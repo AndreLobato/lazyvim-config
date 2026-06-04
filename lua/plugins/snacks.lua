aaa6938079828cc397156811983e783384629d34
AndreLobato <andrefelipelobato@gmail.com>
2026-06-04 15:02:42 +1200

    Improve exclude

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,13 +1,24 @@
+local exclude = {
+  "**/__pycache__/",
+  ".cache",
+  ".git",
+  ".vscode.template",
+  ".venv",
+  ".ruff_cache",
+  ".local",
+  ".pytest_cache"
+}
 return {
   "folke/snacks.nvim",
   opts = {
      picker = {
       sources = {
-        files = { hidden = true, ignored = false, exclude={ "**/__pycache__/", ".cache" }},
-        grep = { hidden = true, ignored = false},
+        files = { hidden = true, ignored = false, exclude=exclude},
+        grep = { hidden = true, ignored = false, exclude=exclude},
         explorer = {
           hidden = true,
           ignored = true,
+          exclude = exclude,
           layout = function()
             return {
               preset = 'sidebar',
